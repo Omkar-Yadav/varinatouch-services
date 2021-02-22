cc1ee7cac957017a30f50049f92bbb5fd53b6739
devp <okeyadav@gmail.com>
2021-02-22 21:33:55 +0530

    added the git api-gate service and modified some files

---
+++ api-gateway/src/test/java/com/variantouch/services/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.variantouch.services.apigateway;
+
+/*
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class ApiGatewayApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
